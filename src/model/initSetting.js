9fdb37641f426c36139f688f566c95f83148af4c
BearD01001 <dino@beard.ink>
2018-01-12 21:28:36 +0800

    Fix initSetting decorator setting initializing error.

--- src/model/initSetting.js
+++ src/model/initSetting.js
@@ -1,10 +1,11 @@
+import { defaultSetting } from '../config';
+
 function initSetting (comp) {
   const compName = comp.name.toLowerCase()
   const newConstructor = function () {
     let setting = localStorage.getItem(`setting-${compName}`)
-
     comp.call(this)
-    setting = setting ? JSON.parse(setting) : void 0
+    setting = setting ? JSON.parse(setting) : defaultSetting[compName]
     if (setting) this.state = setting
   }
 
